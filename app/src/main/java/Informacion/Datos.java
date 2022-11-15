b3d9e99f7f0884d87e8217ed2b494e90067a440e
mannyGL2218 <jonathan2218895@gmail.com>
2022-11-14 23:07:20 -0600

    Parcerables implementados

--- app/src/main/java/Informacion/Datos.java
+++ app/src/main/java/Informacion/Datos.java
@@ -1,6 +1,9 @@
 package Informacion;
 
-public class Datos {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Datos implements Parcelable {
     private String matricula;
     private String nombre;
     private int edad;
@@ -8,7 +11,7 @@ public class Datos {
     private float promedio;
     private String estado;
 
-    public Datos(String matricula, String nombre, int edad, String semestre, float promedio, String estado) {
+    public Datos(String matricula, String nombre, int edad, String semestre, float promedio, String estado){
         this.matricula = matricula;
         this.nombre = nombre;
         this.edad = edad;
@@ -16,6 +19,27 @@ public class Datos {
         this.promedio = promedio;
         this.estado = estado;
     }
+
+    protected Datos(Parcel in){
+        matricula = in.readString();
+        nombre = in.readString();
+        edad = in.readInt();
+        semestre = in.readString();
+        promedio = in.readFloat();
+        estado = in.readString();
+    }
+
+    public static final Creator<Datos> CREATOR = new Creator<Datos>() {
+        @Override
+        public Datos createFromParcel(Parcel in) {
+            return new Datos(in);
+        }
+
+        @Override
+        public Datos[] newArray(int size) {
+            return new Datos[size];
+        }
+    };
 
     public String getMatricula() {
         return matricula;
@@ -63,5 +87,20 @@ public class Datos {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(matricula);
+        parcel.writeString(nombre);
+        parcel.writeInt(edad);
+        parcel.writeString(semestre);
+        parcel.writeFloat(promedio);
+        parcel.writeString(estado);
     }
 }
